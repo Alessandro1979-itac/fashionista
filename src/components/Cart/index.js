cd8f2dcba185ddfdf92b2ce64f3975cd61809f47
Alessandro1979-itac <muniz.caranha@gmail.com>
2020-06-06 16:30:58 -0400

    Pequenas alteracoes para estilizar

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useCallback, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
-import './styles.css';
+//import './styles.css';
 
 import Modal from '../Modal';
 import CartItem from '../CartItem';
