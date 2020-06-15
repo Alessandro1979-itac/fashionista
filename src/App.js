e6e2e8f20f69a29bf2f205293e72aa3eac95c581
Alessandro1979-itac <muniz.caranha@gmail.com>
2020-06-15 12:58:23 -0400

    Pequenos ajustes no arquivo App.js conforme as boas práticas de porgramação

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-import Topbar from './components/Topbar';
+import Header from './components/Header';
 import Routes from './routes';
+
+import Loading from './components/Loading';
 
 import { store, persistor } from './store';
 
@@ -13,7 +15,7 @@ export default function App() {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <Topbar />
+          <Header />
           <Routes />
         </BrowserRouter>
       </PersistGate>
